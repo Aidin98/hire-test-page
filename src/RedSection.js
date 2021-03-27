0d33a5b50c73b8d56ae529394e1dce125f879cdf
Aidin98 <78453568+Aidin98@users.noreply.github.com>
2021-03-27 15:45:58 +0100

    Update RedSection.js

--- src/RedSection.js
+++ src/RedSection.js
@@ -4,7 +4,7 @@ import ad from './images/hire.PNG'
 function RedSection() {
     return (
         <div className='redsection'>
-            	<img src='https://previews.dropbox.com/p/thumb/ABHUzwukQQaOWrYWVYH2P7WO1f8NYub1ptJ4e7RkSwCjWAZ1kZd3CFZpuv8B_lJTlDF9v6pVUo2bmD5cA01rqTPbb-DzTHejxt5FKRBP0zZCzvBpDJe1PyB3D6BCXgQZkHPIFaVSsYcG_31qlmWRhiRcOm_MwVla_GLRRjN6ybB6g9mH1wX_zoj5azs-AOF7eHkgUOJFxRlBpPFl44WCfz8Qrk8maDSoTeR-mp5r6vifd_wEhLWed-aUOplh268PbaO47j7tNZgNzTx9JaMRo6kobCoe_Nef63FwDrDsEXDQXUlA3yT_mcHudFeamNdCuwMYBrbTtbeUqmH5vSpmCUzLkziM4kd20-FsrYfirs2hyg/p.png?fv_content=true&size_mode=5' alt='hiretest'/>
+            	<img src='https://previews.dropbox.com/p/thumb/ABHNqYMeYjfDY9DojN3jKKtlDfIUpuacDQIylYeuboQrGFKmJ_3OCubuGZJwAP1q9WFpIdPvIwwKYzqaXyBf7STOZdfTv1Xf4oTxwZCOAFngx4RpqoLSTJAnLSlN6PnF03iyCPxuckvCzebuAN0D1d7jAQnXra7uH5WHy2hHKvVkfBZtKPpl0JAI9AuqFbnk0Kgy42ePpUu6QqYtXxrkkKat5DvpsQNUOFP-5jCmx-vl6lW25DkknPhVVcVkEC4P3lcdSdSt-TfOAGl7ZuNgEZmn3x-KEsdNT3lf3A3k_p0I5QFNZsIJMaq0rcESaFr9rjJ7t8hdcnP1oyzQlwUQi3a6a3nl9KoXXdaVh9kl9gBJwg/p.png?fv_content=true&size_mode=5' alt='hiretest'/>
                 <h3 className='redsection__h3'>Pellentesque non est at risus</h3>
                 <h1 className='redsection__h1'>30 LOBORTIS</h1>
                 <p className='redsection__p'>DONEC CONSEQUAT QUAM NEQUE, IN MATTIS EX IACULIS. AENEAN {<br/>}IPSUM NEQUE, FEUGIAT EGET PRETIUM, PLACERAT EU LACUS</p>
